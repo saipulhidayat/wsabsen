4ff717cd5e8787053fcecccc54ba6a9adfb4c47d
saipulhidayat <95207125+saipulhidayat@users.noreply.github.com>
2021-11-29 18:46:21 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="refresh" content="0;url=dist/index.html">
+    <title>WAJAN SUNDA RESTO</title>
+    <script language="javascript">
+        window.location.href = "login/index.php";
+    </script>
+</head>
+
+<body>
+</body>
+
+</html>
